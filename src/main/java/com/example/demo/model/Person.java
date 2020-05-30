d799560f61769a4ab1b0da5ee33c797fdb30307d
jirwincline <jci@pdx.edu>
2020-05-29 17:11:10 -0700

    create spring projects and person Model

---
+++ src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+public class Person {
+
+  private final UUID id;
+  private final String name;
+
+  public Person(UUID id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+}
