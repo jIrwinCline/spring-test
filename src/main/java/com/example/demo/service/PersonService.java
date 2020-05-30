ac76d632afb76e64092387d5a84bdee3bc2a1428
jirwincline <jci@pdx.edu>
2020-05-29 21:50:02 -0700

    add person to mock database with postman

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.dao.PersonDao;
 import com.example.demo.model.Person;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,7 @@ public class PersonService {
   public int addPerson(Person person) {
     return personDao.insertPerson(person);
   }
-
+//  public List<Person> getAllPeople() {
+//    return personDao.selectAllPeople();
+//  }
 }
