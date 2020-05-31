377a402800782a0f2b7ed9782d8b12213af1f19a
jirwincline <jci@pdx.edu>
2020-05-31 01:17:42 -0700

    Add rest routes to delete and update user

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +39,13 @@ public class PersonController {
   public Person getPersonById(@PathVariable("id") UUID id) {
     return personService.getPersonById(id)
             .orElse(null);
+  }
+  @DeleteMapping(path = "{id}")
+  public void deletePersonById(@PathVariable("id") UUID id) {
+    personService.deletePerson(id);
+  }
+  @PutMapping
+  public void updatePerson(@PathVariable("{id}") UUID id, @RequestBody Person personToUpdate) {
+    personService.updatePerson(id, personToUpdate);
   }
 }
